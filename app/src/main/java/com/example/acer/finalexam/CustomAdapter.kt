965373757d9488d3b888e7eff526dc841aa024ce
Julius Barde <yam1434414344@gmail.com>
2018-03-21 12:57:24 -0700

    second commit

--- app/src/main/java/com/example/acer/finalexam/CustomAdapter.kt
+++ app/src/main/java/com/example/acer/finalexam/CustomAdapter.kt
@@ -1,5 +1,40 @@
 package com.example.acer.finalexam
 
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+
 /**
  * Created by acer on 3/21/2018.
  */
+class CustomAdapter(val albumList:ArrayList<Album_Model>,context: Context) : RecyclerView.Adapter<CustomAdapter.ViewHolder>(){
+    var mcontext=context
+    override fun getItemCount(): Int {
+        return albumList.size
+    }
+
+    class ViewHolder(itemView: View) :RecyclerView.ViewHolder(itemView) {
+        val albumName= itemView.findViewById<TextView>(R.id.albumName_tv)
+        val artistName= itemView.findViewById<TextView>(R.id.artistName_tv)
+        val albumImage = itemView.findViewById<ImageView>(R.id.albumImage)
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(parent?.context).inflate(R.layout.album_item,parent,false)
+        return ViewHolder(v)
+    }
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        val album:Album_Model = albumList[position]
+        holder?.albumName?.text = album.albumTitle
+        holder?.artistName?.text = album.artistName
+       // val pokemonImage = holder?.pokeImage
+       // Picasso.with(mcontext).load(pokemon.pokeImage).into(pokemonImage)
+
+
+
+    }
+}
--- app/src/main/java/com/example/acer/finalexam/MainActivity.kt
+++ app/src/main/java/com/example/acer/finalexam/MainActivity.kt
@@ -1,14 +1,17 @@
-package com.example.acer.finalexam
+    package com.example.acer.finalexam
 
-import android.support.v7.app.AppCompatActivity
-import android.os.Bundle
+    import android.support.v7.app.AppCompatActivity
+    import android.os.Bundle
+    import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+    class MainActivity : AppCompatActivity() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+
+        }
+
+
     }
-
-
-}
